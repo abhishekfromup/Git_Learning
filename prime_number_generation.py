ebd645c07b37d24ae9591b803d1a98d5d1cc2ffd
Gaurav <abhishek.singh.201194@gmail.com>
2018-07-20 14:20:39 +0530

    Adding program prime_number_generation.py

---
+++ prime_number_generation.py
@@ -0,0 +1,24 @@
+# test.py
+import math
+# from math import sqrt
+
+def primeNumberGenerator(limit):
+	global total
+	for i in range(2, limit):
+		for j in range(2, int(math.sqrt(i))+1):
+			if ((i % j) == 0):
+			# print("%d is NOT prime number \n" %i)
+				break
+		else:
+			print(i , "is prime number")
+			total += 1
+
+def main():
+	global total, limit
+	total = 0
+	limit = int(input("Please enter the limit till where you want to generate prime numbers: "))
+	primeNumberGenerator(limit)
+	print("Total prime numbers till ", limit, " are: " , total)
+
+if (__name__ == "__main__"):
+	main()
